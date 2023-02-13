008dbe844125827688c9d53837c613ab34298dc7
GAJANAN270395 <108356887+GAJANAN270395@users.noreply.github.com>
2023-02-13 20:04:48 +0530

    2 files modified, 4 added

---
+++ src/pages/SingleJobDetails.jsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+const SingleJobDetails = ()=>{
+  return(
+    <>
+    <div> SingleJobDetails </div>;
+    </>
+  )
+}
+export default SingleJobDetails;
--- src/router/Routers.jsx
+++ src/router/Routers.jsx
@@ -1,10 +1,22 @@
-
 import React from "react";
+import {Routes,Route, Navigate} from "react-router-dom"
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import SingleJobDetails from "../pages/SingleJobDetails";
+
+
 const Routers = ()=>{
   return(
-    <>
-    <div> Routers </div>;
-    </>
+   
+     <Routes>
+       <Route path="/" element={<Navigate to="/home"/>} />
+       <Route path="/login" element={<Home/>} />
+       <Route path="/login" element={<Login/>} />
+       <Route path="/login" element={<Register/>} />
+       <Route path="/login" element={<SingleJobDetails/>} />
+     </Routes>
+  
   ) 
 }
 export default Routers;
