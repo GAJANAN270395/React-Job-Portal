fc81f3f9bdeea0a1727cc1cd9305597a7e4e2a9e
GAJANAN270395 <108356887+GAJANAN270395@users.noreply.github.com>
2023-02-13 19:39:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React-Job-Portal
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-qhxfxm)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Button} from 'reactstrap';
+import "./styles/style.scss";
+
+const App =()=>{
+  return(
+    <>
+    <h1>Job Portal</h1>
+    <Button className="btn primary__btn">Hello</Button>
+    </>
+  )
+}
+export default App;
