a5204928a88acdbc47ede9f8e27396f45361f0cc
GAJANAN270395 <108356887+GAJANAN270395@users.noreply.github.com>
2023-02-15 08:57:34 +0530

    1 file modified, 1 added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,39 @@
 
 import React from "react";
+import {Container, Row, Button} from "reactstrap"
+import {NavLink, Link} from "react-router-dom"
+import './Header.scss';
+import {logo} from 
+
+const nav_links = [
+  {
+    path: "/",
+    display: "Home"
+  },
+  {
+    path: "/jobs",
+    display: "Jobs",
+  },
+  {
+    path: "/login",
+    display: "Login",
+  },
+  {
+    path: "/register",
+    display: "Register",
+  },
+];
+
 const Header = ()=>{
   return(
-    <>
-    <div> Header </div>
-    </>
+    <header className="header">
+   <Container>
+     <Row>
+      <div className=".nav_wrapper.d-flex align-items center justify-content-between">
+      </div>
+     </Row>
+   </Container>
+    </header>
   )
 }
 export default Header;
