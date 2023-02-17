e6c0b6ce70b446032af023e6bb7532a9583668ca
GAJANAN270395 <108356887+GAJANAN270395@users.noreply.github.com>
2023-02-17 10:09:03 +0530

    2 files modified

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,67 +1,47 @@
 
 import React from "react";
-import {Container, Row, Button,Nav,NavBar, NavDropdown} from "reactstrap"
-import {NavLink, Link} from "react-router-dom"
+import {Container,  Button} from "react-bootstrap"
+import {NavLink,Link} from "react-router-dom"
 import './Header.scss';
 import logo from "../../assets/images/logo.png";
 
-const nav_links = [
+const navLinks=[
   {
-    path: "/",
-    display: "Home"
+    path:"/jobs",
+    display:"Search Job"
   },
   {
-    path: "/jobs",
-    display: "Jobs", 
+  path:"/internship",
+  display:"Internship"
   },
   {
-    path: "/login",
-    display: "Login",
-  },
-  {
-    path: "/register",
-    display: "Register",
-  },
-];
+    path:"/post-job",
+    display: "Post a job"
+  }
+]
 
 const Header = ()=>{
   return(
-    <header className="header">
-   <Container>
-     <Row>
-      <div className=".nav_wrapper.d-flex align-items center justify-content-between">
-        <div className="logo">
-          <img src="https://stackblitz.com/files/react-qhxfxm/github/GAJANAN270395/React-Job-Portal/main/src/assets/logo.png" alt=""/>
-        </div>
-        <div className="navigation">
-          <ul className="menu d-flex align-items-center gap-5">
-{nav_links.map((item, index)=>{
-  <li className="nav__item">
-     <NavLink
-     to ={item.path} className={(navClass)=>
-        navClass.isActive ? "active__link" : ""
-    }
-     >
-       {item.display}
-     </NavLink>
-  </li>;
-})}
-          </ul>
-        </div>
-        <div className="nav__right d-flex align-items-center gap-4">
-          <div className="nav__btn">
-            <Button className="btn primary__btn">
-              <Link to="/login">Login</Link>
-            </Button>
-            <Button className="btn primary__btn">
-              <Link to="/register">Register</Link>
-            </Button>
-          </div>
-        </div>
+      <header className="header">
+    <Container>
+      <div className="left_menu_section">
+       <div className="logo">
+         <Link to="/">
+           <img src="https://stackblitz.com/files/react-qhxfxm/github/GAJANAN270395/React-Job-Portal/main/src/assets/logo.png" alt="" className="logo_img"/>
+         </Link>
+       </div>
       </div>
-     </Row>
-   </Container>
-    </header>
+      <div className="right_menu_section">
+        <ul className="nav_menu">
+           {navLinks.map((item, index)=>(
+             <li className="nav_item">
+             <NavLink to={item.path} >{item.display}</NavLink>
+             </li>
+           ))}
+          </ul>
+      </div>
+    </Container>
+      </header>
   )
 }
 export default Header;
